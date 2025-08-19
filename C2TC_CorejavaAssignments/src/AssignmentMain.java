606e2dc7ab3372d31fd3fe80d1bf9cc4da534d45
Iswariya-14 <iswariya1403@gmail.com>
2025-08-19 15:24:04 +0530

    commit

---
+++ C2TC_CorejavaAssignments/src/AssignmentMain.java
@@ -0,0 +1,21 @@
+import com.tnsif.assignmentone.employees.*;
+import com.tnsif.assignmentone.utilities.EmployeeUtilities;
+public class AssignmentMain 
+{
+public static void main(String[] args) 
+{
+        
+        // Create Manager and Developer objects
+        Manager mgr = new Manager("Alice", 101, 80000, "HR");
+        Developer dev = new Developer("Bob", 102, 60000, "Java");
+
+        // Use utility methods
+        EmployeeUtilities.showEmployeeDetails(mgr);
+        EmployeeUtilities.giveBonus(mgr, 5000);
+
+        System.out.println("------------------");
+
+        EmployeeUtilities.showEmployeeDetails(dev);
+        EmployeeUtilities.giveBonus(dev, 3000);
+    }
+}
---
+++ C2TC_CorejavaAssignments/src/com/tnsif/assignmentone/utilities/EmployeeUtilities.java
@@ -0,0 +1,16 @@
+package com.tnsif.assignmentone.utilities;
+import com.tnsif.assignmentone.employees.Employee;
+public class EmployeeUtilities 
+{
+    // Method to give bonus
+    public static void giveBonus(Employee emp, double bonusAmount) {
+        double newSalary = emp.getSalary() + bonusAmount;
+        System.out.println("Bonus of " + bonusAmount + " added to " + emp.getName());
+        System.out.println("New Salary: " + newSalary);
+    }
+
+    // Method to display employee info using polymorphism
+    public static void showEmployeeDetails(Employee emp) {
+        emp.displayInfo();
+    }
+}
