36eb6b2558d28e9d677097ca0917cd9d91ed0e95
Iswariya-14 <iswariya1403@gmail.com>
2025-08-19 18:47:36 +0530

    commit

---
+++ C2TC_CorejavaAssignments/src/com/tnsif/assignmenttwo/AssignmenttwoMain.java
@@ -0,0 +1,14 @@
+package com.tnsif.assignmenttwo;
+
+public class AssignmenttwoMain 
+{
+	public static void main(String[] args) {
+        // Part 1: Student object
+        Student s1 = new Student(); // will print message
+
+        // Part 2: Commission object
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+    }
+}
